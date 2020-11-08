9aa6ebecfc6674ccf2197ac3b167f8d9219b35fa
Audrey Schmitt <audrey.schmitt07@gmail.com>
2020-11-07 21:04:52 -0500

    Added snake growth

--- Snake/Assets/Scripts/Player.cs
+++ Snake/Assets/Scripts/Player.cs
@@ -11,8 +11,13 @@ public class Player : MonoBehaviour
     
     /* Serialized private fields */
     [SerializeField] private float speed = 1f;
+    [SerializeField] private GameObject snakeBodyPrefab = null;
+    [SerializeField] private Transform snakeBodyParent = null;
 
     /* Private fields */
+    private int _size = 0;                    // size of 0 means just head
+    private List<Vector3> _snakePositionList;
+    private List<GameObject> _snakeParts;
     private Direction _currentDirection;
     private enum Direction
     {
@@ -23,6 +28,13 @@ public class Player : MonoBehaviour
     }
 
 
+    /* Initialize necessary fields */
+    void Awake()
+    {
+        _snakePositionList = new List<Vector3>();
+        _snakeParts = new List<GameObject>();
+    }
+    
     /* Start player movement coroutine */
     void Start()
     {
@@ -47,6 +59,13 @@ public class Player : MonoBehaviour
         int x = (int) transform.position.x;
         int y = (int) transform.position.y;
         
+        _snakePositionList.Insert(0, new Vector3(x, y, 0));
+
+        if (_snakePositionList.Count > _size + 1)
+        {
+            _snakePositionList.RemoveAt(_snakePositionList.Count - 1);
+        }
+
         switch (_currentDirection)
         {
             case (Direction.Down):
@@ -63,7 +82,14 @@ public class Player : MonoBehaviour
                 break;
         }
 
-        transform.position = new Vector2(x, y);
+        transform.position = new Vector2(x, y);                // Move snake head
+        
+        /* Move rest of snake body */
+        for (int i = 0; i < _snakeParts.Count; i++)
+        {
+            _snakeParts[i].transform.position = _snakePositionList[i];
+        }
+
     }
 
     /* Move player according to speed */
@@ -103,13 +129,28 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
     }
 
-    /* If player collides with an apple, destroy it and spawn a new apple */
+    /* If player collides with an apple, destroy it and spawn a new apple. If collides with snakeBody, destroy snake */
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Apple")
         {
             Destroy(other.gameObject);
             OnAppleEaten.Invoke();
+            ExtendSnake();
         }
+
+        if (other.gameObject.tag == "SnakeBody")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    /* Instantiate more instances of snakeBody, making snake longer */
+    private void ExtendSnake()
+    {
+        _size++;
+        GameObject newSnakePart = Instantiate(snakeBodyPrefab, snakeBodyParent);
+        newSnakePart.transform.position = _snakePositionList[_snakePositionList.Count - 1];
+        _snakeParts.Insert(_snakeParts.Count, newSnakePart);
     }
 }
